c3f868d94d34ee78f7b104c6741a5d83ade57e58
Surendharan Baskaran <surendharanbe@gmail.com>
2023-04-12 03:06:08 +0530

    Full Working

--- E1904/Deploy/latest/new/urls.py
+++ E1904/Deploy/latest/new/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home_view,name='home_view'),
+    path('', views.home_view,name='home_view'),
     path('predict/',views.predict_view,name='predict'),
     path('user_signup/',views.user_register,name='user_register'),
     path('signup/',views.register,name='register'),
     path('login/',views.login,name='login'),
-    path('doctor_feedback/',views.feedback,name='feedback'),
+    path('officer_feedback/',views.feedback,name='feedback'),
     path('index/',views.apredict,name='apredict'),
     path('logout/',views.logout_view,name='logout'),
-    path('last_patients/',views.view_last,name='details_last'),
-    path('all_patients/',views.view_all,name='details_all'),
+    path('last_users/',views.view_last,name='details_last'),
+    path('all_users/',views.view_all,name='details_all'),
 ]
